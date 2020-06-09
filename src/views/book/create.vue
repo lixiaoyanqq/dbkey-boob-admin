91a9a5a50dca1f1aee374ae4413b841f1444c05e
李小彦 <dbkey@lixiaoyandeMacBook-Pro.local>
2020-06-09 10:37:05 +0800

    上传电子书功能

---
+++ src/views/book/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <detail :is-edit="false" />
+</template>
+<script>
+import Detail from './components/Detail'
+export default {
+  components: {
+    Detail
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/book/list.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    图书列表
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
